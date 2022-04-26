9d208ee1761ba06fc5cba0ec8378510b20034974
Jose Salas <jsalvadorsalas000@gmail.com>
2022-04-25 23:54:38 -0700

    fixed rules.js

--- rules.js
+++ rules.js
@@ -7,18 +7,19 @@ class Start extends Scene {
 
     handleChoice() {
         this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
-        console.log(this.engine.storyData.InitialLocation);
+        //console.log(this.engine.storyData.InitialLocation);
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = this.engine.storyData.Locations; // TODO: use `key` to get the data object for the current story location
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        console.log(locationData)
         this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
         
         if("Choices" in locationData) { // TODO: check if the location has any Choices
             for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
-                this.engine.addChoice(choice.Text, choice.Target); // TODO: use the Text of the choice
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
