b97ebe7b471d84c332707fdfeaa665b24e8a73d4
t4ylo <thpearce@ucsc.edu>
2025-05-11 00:21:57 -0600

    final2

--- rules.js
+++ rules.js
@@ -1,60 +1,53 @@
 class Start extends Scene {
     create() {
-<<<<<<< Updated upstream
-        this.engine.show(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
-=======
-        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
->>>>>>> Stashed changes
+        this.engine.setTitle(this.engine.storyData.Title); // Set the story title
         this.engine.addChoice("Begin the story");
-        //console.log(this.engine.storyData.Title);
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
-<<<<<<< Updated upstream
-        //console.log(this.engine.storyData.InitialLocation);
-=======
->>>>>>> Stashed changes
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // Go to the initial location
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
-<<<<<<< Updated upstream
-        //console.log(locationData)
-        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
-        
-        if("Choices" in locationData) { // TODO: check if the location has any Choices
-=======
-        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
-        
-        if(locationData.Choices && locationData.Choices.length > 0) { // TODO: check if the location has any Choices
->>>>>>> Stashed changes
-            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
-                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let locationData = this.engine.storyData.Locations[key]; // Get data for current location
+        let bodyText = locationData.Body;
+        if (bodyText.includes("{{RANDOM_TAPE_MESSAGE}}")) {
+            let options = this.engine.storyData.RandomTapeMessages;
+            let randomMessage = options[Math.floor(Math.random() * options.length)];
+            bodyText = bodyText.replace("{{RANDOM_TAPE_MESSAGE}}", randomMessage);
+        }
+        this.engine.show(bodyText);
+
+        if (locationData.Choices && locationData.Choices.length > 0) {
+            for (let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice); // Store entire choice object
             }
         } else {
-            this.engine.addChoice("La Fin")
+            this.engine.addChoice("La Fin");
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+        if (choice && choice.Target) {
+            this.engine.show("&gt; " + choice.Text);
+            this.engine.gotoScene(Location, choice.Target); // Move to next scene
         } else {
-            this.engine.gotoScene(End);
+            this.engine.gotoScene(End); // End of story
         }
     }
+
+    
 }
 
 class End extends Scene {
     create() {
         this.engine.show("<hr>");
-        this.engine.show(this.engine.storyData.Credits);
+        this.engine.show(this.engine.storyData.Credits); // Show the credits
     }
 }
+
+
 
 Engine.load(Start, 'myStory.json');
